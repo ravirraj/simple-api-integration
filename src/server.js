05ba8a0d86d7d28a9ded98a4f888fd9aa47d06ed
yami <ravirrajpatil@gmail.com>
2025-12-10 23:07:26 +0530

    add dotenv

--- src/server.js
+++ src/server.js
@@ -1,6 +1,10 @@
 import app from "./app.js";
+import dotenv from "dotenv";
+
+dotenv.config({path: "./src/.env"}); //put you dot env in root directory
 
 const PORT = process.env.PORT || 3000;
+console.log(process.env.PORT )
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
