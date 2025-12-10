cb6def04c060b1dde991bb436d3f79f864cdfc73
yami <ravirrajpatil@gmail.com>
2025-12-10 23:07:55 +0530

    removed some code

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config({path: "./src/.env"}); //put you dot env in root directory
 
 const PORT = process.env.PORT || 3000;
-console.log(process.env.PORT )
+
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
